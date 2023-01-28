3019f6de19489e3009039a072a5969c864564426
Asim <asim_99@hotmail.co.uk>
2023-01-28 22:45:30 +0000

    Added function for lighting logs

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
-import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
+import java.util.Scanner;
 
 public class Main {
-    Scanner scanner = new Scanner(System.in);
+
 
     public static void main(String[] args) {
         charCreation();
         firemakingFishingTutorial();
-
     }
 
     //character creation method
@@ -19,16 +18,13 @@ public class Main {
         String playerName = scanner.nextLine();
 
         System.out.println("Welcome to the world of runescape, " + playerName + "!");
-
     }
 
     public static void firemakingFishingTutorial() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("My name is Brianna! Here's an axe and a tinderbox, cut a nearby tree!");
 
-
         boolean keepAsking = true;
-
 
         while (keepAsking) {
             System.out.println("You see a nearby tree, do you want to cut it? (Y/N)");
@@ -38,21 +34,34 @@ public class Main {
                 System.out.println("You swing your axe at the tree.");
                 try {
                     TimeUnit.SECONDS.sleep(1);
-
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
                 System.out.println("You receive 1 log");
                 keepAsking = false;
-
             } else if (input.equalsIgnoreCase("N")) {
                 System.out.println("You decided not to cut the tree.");
-                System.out.println("Talk to Brianna, again? Y/N");
-
-
             }
         }
+        System.out.println("Brianna: Great! Use your tinderbox on the logs to create fire.");
 
+        boolean keepAsking3 = true;
+        while (keepAsking3) {
+            System.out.println("Press 1 to use your tinderbox to light the logs.");
+            String lightLogs = scanner.nextLine();
 
+            if (lightLogs.equals("1")) {
+                System.out.println("Lighting the logs...");
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("You create a fire!");
+                keepAsking3 = false;
+            } else {
+                System.out.println(lightLogs + " is an invalid choice.");
+            }
+        }
     }
 }
